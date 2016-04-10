6de3ec34b0deb65cb819119d1a9465c45ed4929e
Brian Falor <brian.falor@gmail.com>
2016-04-09 19:21:26 -0600

    Created this repo to do a countdown from 10 to 0 for our rocket launching -So far I have the countdown light and buzzer working.

---
+++ LaunchButton.ino
@@ -0,0 +1,35 @@
+const int Button = 2;
+const int LED =  3;
+int buttonState = 0;
+
+int sound = 400; //Pitch for the piezo
+int count = 500; //Time for the countdown
+
+void setup() {
+  pinMode(LED, OUTPUT);
+  pinMode(Button, INPUT);
+}
+
+void loop() {
+  buttonState = digitalRead(Button);
+
+  if (buttonState == HIGH){
+    countdown();
+  } 
+  else{
+    digitalWrite(LED, LOW);
+  }
+}
+
+void countdown(){
+  for (int i = 1; i<=10; i++)
+  {
+    digitalWrite(LED, HIGH);
+    tone(9, sound);
+    delay(count);
+    digitalWrite(LED, LOW);
+    noTone(9);
+    delay(count);
+  }
+}
+
